7675e9bec3389b8951301196fee4bb498bf70bba
Hary Pirajan Mahalingam <hary2309@gmail.com>
2023-11-18 02:59:05 +0100

    Remove unneeded code

--- src/extension.js
+++ src/extension.js
@@ -1,180 +1,129 @@
-import Meta from "gi://Meta";
-import St from "gi://St";
-import GLib from "gi://GLib";
+import Meta from 'gi://Meta';
+import St from 'gi://St';
+import GLib from 'gi://GLib';
 
-import { Extension } from "resource:///org/gnome/shell/extensions/extension.js";
-import * as Main from "resource:///org/gnome/shell/ui/main.js";
-import { SettingsManager } from "./managers/settingsManager.js";
-import { StylingClassManager } from "./managers/stylingClassManager.js";
-import { ShellEventManager } from "./managers/shellEventManager.js";
+import {Extension} from 'resource:///org/gnome/shell/extensions/extension.js';
+import * as Main from 'resource:///org/gnome/shell/ui/main.js';
+import {SettingsManager} from './managers/settingsManager.js';
+import {StylingClassManager} from './managers/stylingClassManager.js';
+import {ShellEventManager} from './managers/shellEventManager.js';
 
 const config = {
-  extensionId: "com.ftpix.transparentbar",
-  colorSchemes: ["light", "dark"],
+    extensionId: 'com.ftpix.transparentbar',
+    colorSchemes: ['light', 'dark'],
 };
 
 export default class TransparentTopBarWithCustomTransparencyExtension extends Extension {
-  constructor(metadata) {
-    super(metadata);
-    this._actorSignalIds = null;
-    this._windowSignalIds = null;
-    this._delayedTimeoutId = null;
-    this.transparencyChangeDebounce = null;
-    this.darkFullScreenChangeDebounce = null;
-  }
-
-  enable() {
-    this._stylingClassManager = new StylingClassManager();
-    this._settings = new SettingsManager(this.getSettings(config.extensionId));
-    this._shellEventManager = new ShellEventManager();
-
-    this._actorSignalIds = new Map();
-    this._windowSignalIds = new Map();
-    this._settings.onChange((key) => this.transparencyChanged(key));
-    this._actorSignalIds.set(Main.overview, [
-      Main.overview.connect("showing", this._updateTransparent.bind(this)),
-      Main.overview.connect("hiding", this._updateTransparent.bind(this)),
-    ]);
-
-    this._actorSignalIds.set(Main.sessionMode, [
-      Main.sessionMode.connect("updated", this._updateTransparent.bind(this)),
-    ]);
-
-    this._shellEventManager.onWindowPositionChange(
-      this._updateTransparent.bind(this)
-    );
-
-    //Use a delayed version of _updateTransparent to let the shell catch up
-    this._shellEventManager.onWorkspaceSwitch(
-      this._updateTransparentDelayed.bind(this)
-    );
-
-    this._shellEventManager.onColorSchemeChange(
-      this._updateTransparent.bind(this)
-    );
-
-    this._updateTransparent();
-  }
-
-  transparencyChanged(key) {
-    if (
-      key === SettingsManager.KEYS.OPACITY_DARK ||
-      key === SettingsManager.KEYS.OPACITY_LIGHT
-    ) {
-      GLib.source_remove(this.transparencyChangeDebounce);
-      this.transparencyChangeDebounce = GLib.timeout_add(
-        GLib.PRIORITY_DEFAULT,
-        250,
-        () => this._updateTransparent()
-      );
-      return;
+    constructor(metadata) {
+        super(metadata);
     }
 
-    if (key === SettingsManager.KEYS.DISABLE_ON_FULLSCREEN) {
-      GLib.source_remove(this.darkFullScreenChangeDebounce);
-      this.darkFullScreenChangeDebounce = GLib.timeout_add(
-        GLib.PRIORITY_DEFAULT,
-        250,
-        () => this._updateTransparent()
-      );
-      return;
-    }
-  }
+    enable() {
+        this._stylingClassManager = new StylingClassManager();
+        const settings = this.getSettings(config.extensionId);
+        this._settingsManager = new SettingsManager(settings);
+        this._shellEventManager = new ShellEventManager();
 
-  disable() {
-    GLib.source_remove(this.transparencyChangeDebounce);
-    GLib.source_remove(this.darkFullScreenChangeDebounce);
+        this._setupListeners();
 
-    for (const actorSignalIds of [
-      this._actorSignalIds,
-      this._windowSignalIds,
-    ]) {
-      for (const [actor, signalIds] of actorSignalIds) {
-        for (const signalId of signalIds) {
-          actor.disconnect(signalId);
-        }
-      }
-    }
-    this._actorSignalIds = null;
-    this._windowSignalIds = null;
-
-    if (this._delayedTimeoutId != null) {
-      GLib.Source.remove(this._delayedTimeoutId);
-    }
-    this._delayedTimeoutId = null;
-
-    this._stylingClassManager.disableAll();
-    this._shellEventManager.disconnect();
-    this._settings = null;
-  }
-  _updateTransparentDelayed() {
-    this._delayedTimeoutId = GLib.timeout_add(
-      GLib.PRIORITY_DEFAULT,
-      100,
-      () => {
+        this._stylingClassManager.enableBaseClass();
         this._updateTransparent();
+    }
+
+    disable() {
+        if (this._delayedTimeoutId != null) {
+            GLib.Source.remove(this._delayedTimeoutId);
+        }
         this._delayedTimeoutId = null;
-        return GLib.SOURCE_REMOVE;
-      }
-    );
-  }
-
-  _isInOverview() {
-    return Main.panel.has_style_pseudo_class("overview");
-  }
-
-  _isAnyWindowNearTopBar() {
-    if (!Main.sessionMode.hasWindows) return false;
-
-    // Get all the windows in the active workspace that are in the primary monitor and visible.
-    const workspaceManager = global.workspace_manager;
-    const activeWorkspace = workspaceManager.get_active_workspace();
-    const windows = activeWorkspace.list_windows().filter((metaWindow) => {
-      return (
-        metaWindow.is_on_primary_monitor() &&
-        metaWindow.showing_on_its_workspace() &&
-        !metaWindow.is_hidden() &&
-        metaWindow.get_window_type() !== Meta.WindowType.DESKTOP &&
-        !metaWindow.skip_taskbar
-      );
-    });
-
-    // Check if at least one window is near enough to the panel.
-    const panelTop = Main.panel.get_transformed_position()[1];
-    const panelBottom = panelTop + Main.panel.get_height();
-    const scale = St.ThemeContext.get_for_stage(global.stage).scale_factor;
-    const isNearEnough = windows.some((metaWindow) => {
-      const verticalPosition = metaWindow.get_frame_rect().y;
-      return verticalPosition < panelBottom + 5 * scale;
-    });
-
-    return isNearEnough;
-  }
-
-  _updateTransparent() {
-    const disableForMaximizedWindow =
-      this._settings.getDisableOnFullscreen() && this._isAnyWindowNearTopBar();
-
-    if (this._isInOverview() || disableForMaximizedWindow) {
-      this._stylingClassManager.disableAll();
-      return;
+        this._stylingClassManager.disableAll();
+        this._shellEventManager.disconnect();
+        this._settingsManager = null;
     }
 
-    const colorScheme = this._getTheme();
-    const transparency = this._settings.getTransparencyDark();
-    this._stylingClassManager.enableBaseClass();
-    this._stylingClassManager.enableColorSpecificClass(colorScheme);
-    this._stylingClassManager.enableOpacitySpecificClass(
-      colorScheme,
-      transparency
-    );
-  }
+    _setupListeners() {
+        this._settingsManager.onChange(
+            this._updateTransparentDelayed.bind(this)
+        );
 
-  _getTheme() {
-    if (Main.sessionMode.colorScheme !== "prefer-light") {
-      return "dark";
+        this._shellEventManager.onWindowPositionChange(
+            this._updateTransparent.bind(this)
+        );
+
+        //Use a delayed version of _updateTransparent to let the shell catch up
+        this._shellEventManager.onWorkspaceSwitch(
+            this._updateTransparentDelayed.bind(this)
+        );
+
+        this._shellEventManager.onColorSchemeChange(
+            this._updateTransparent.bind(this)
+        );
     }
-    const { colorScheme } = St.Settings.get();
-    return colorScheme === St.SystemColorScheme.PREFER_DARK ? "dark" : "light";
-  }
+
+    _isAnyWindowNearTopBar() {
+        if (!Main.sessionMode.hasWindows) return false;
+
+        // Get all the windows in the active workspace that are in the primary monitor and visible.
+        const workspaceManager = global.workspace_manager;
+        const activeWorkspace = workspaceManager.get_active_workspace();
+        const windows = activeWorkspace.list_windows().filter(metaWindow => {
+            return (
+                metaWindow.is_on_primary_monitor() &&
+                metaWindow.showing_on_its_workspace() &&
+                !metaWindow.is_hidden() &&
+                metaWindow.get_window_type() !== Meta.WindowType.DESKTOP &&
+                !metaWindow.skip_taskbar
+            );
+        });
+
+        // Check if at least one window is near enough to the panel.
+        const panelTop = Main.panel.get_transformed_position()[1];
+        const panelBottom = panelTop + Main.panel.get_height();
+        const scale = St.ThemeContext.get_for_stage(global.stage).scale_factor;
+        const isNearEnough = windows.some(metaWindow => {
+            const verticalPosition = metaWindow.get_frame_rect().y;
+            return verticalPosition < panelBottom + 5 * scale;
+        });
+
+        return isNearEnough;
+    }
+
+    _updateTransparent() {
+        const disableForMaximizedWindow =
+            this._settingsManager.getDisableOnFullscreen() &&
+            this._isAnyWindowNearTopBar();
+
+        if (disableForMaximizedWindow) {
+            this._stylingClassManager.disableColoringClass();
+            return;
+        }
+
+        const colorScheme = this._getTheme();
+        const transparency = this._settingsManager.getTransparencyDark();
+        this._stylingClassManager.enableColoringClass(
+            colorScheme,
+            transparency
+        );
+    }
+
+    _updateTransparentDelayed() {
+        this._delayedTimeoutId = GLib.timeout_add(
+            GLib.PRIORITY_DEFAULT,
+            250,
+            () => {
+                this._updateTransparent();
+                this._delayedTimeoutId = null;
+                return GLib.SOURCE_REMOVE;
+            }
+        );
+    }
+
+    _getTheme() {
+        if (Main.sessionMode.colorScheme !== 'prefer-light') {
+            return 'dark';
+        }
+        const {colorScheme} = St.Settings.get();
+        return colorScheme === St.SystemColorScheme.PREFER_DARK
+            ? 'dark'
+            : 'light';
+    }
 }
--- src/managers/stylingClassManager.js
+++ src/managers/stylingClassManager.js
@@ -1,79 +1,74 @@
-import * as Main from "resource:///org/gnome/shell/ui/main.js";
+import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 
 export class StylingClassManager {
-  static KEYS = {
-    BASE_CLASS: "baseClass",
-    COLOR_SPECIFIC: "colorSpecific",
-    OPACITY_SPECIFIC: "opacitySpecific",
-  };
-  constructor() {
-    this._enabledMainPanelClasses = {};
-  }
-
-  /**
-   * @param {string} key
-   * @param {string} className
-   */
-  _addMainPanelClass(key, className) {
-    this._removeMainPanelClass(key);
-    this._enabledMainPanelClasses[key] = className;
-    Main.panel.add_style_class_name(className);
-  }
-
-  /**
-   * @param {string} key
-   */
-  _removeMainPanelClass(key) {
-    const className = this._enabledMainPanelClasses[key];
-    if (className) {
-      Main.panel.remove_style_class_name(className);
+    static KEYS = {
+        BASE_CLASS: 'baseClass',
+        COLOR_SPECIFIC: 'colorSpecific',
+        OPACITY_SPECIFIC: 'opacitySpecific',
+    };
+    constructor() {
+        this._enabledMainPanelClasses = {};
     }
-    delete this._enabledMainPanelClasses[key];
-  }
 
-  enableBaseClass() {
-    this._addMainPanelClass(
-      StylingClassManager.KEYS.BASE_CLASS,
-      "transparent-top-bar"
-    );
-  }
-
-  disableBaseClass() {
-    this._removeMainPanelClass(StylingClassManager.KEYS.BASE_CLASS);
-  }
-
-  /**
-   * @param {string} colorScheme
-   */
-  enableColorSpecificClass(colorScheme) {
-    this._addMainPanelClass(
-      StylingClassManager.KEYS.COLOR_SPECIFIC,
-      "transparent-top-bar-" + colorScheme
-    );
-  }
-
-  disableColorSpecificClass() {
-    this._removeMainPanelClass(StylingClassManager.KEYS.COLOR_SPECIFIC);
-  }
-
-  /**
-   * @param {string} colorScheme
-   * @param {number} transparency
-   */
-  enableOpacitySpecificClass(colorScheme, transparency) {
-    this._addMainPanelClass(
-      StylingClassManager.KEYS.OPACITY_SPECIFIC,
-      "transparent-top-bar-" + colorScheme + "-" + transparency
-    );
-  }
-
-  disableOpacitySpecificClass() {
-    this._removeMainPanelClass(StylingClassManager.KEYS.OPACITY_SPECIFIC);
-  }
-
-  disableAll() {
-    for (const key of Object.keys(this._enabledMainPanelClasses)) {
-      this._removeMainPanelClass(key);
+    /**
+     * @param {string} key
+     * @param {string} className
+     */
+    _addMainPanelClass(key, className) {
+        if (this._enabledMainPanelClasses[key] === className) {
+            return;
+        }
+        Main.panel.add_style_class_name(className);
+        this._removeMainPanelClass(key);
+        this._enabledMainPanelClasses[key] = className;
     }
-  }
+
+    /**
+     * @param {string} key
+     */
+    _removeMainPanelClass(key) {
+        const className = this._enabledMainPanelClasses[key];
+        if (className) {
+            Main.panel.remove_style_class_name(className);
+        }
+        delete this._enabledMainPanelClasses[key];
+    }
+
+    enableBaseClass() {
+        this._addMainPanelClass(
+            StylingClassManager.KEYS.BASE_CLASS,
+            'transparent-top-bar'
+        );
+    }
+
+    disableBaseClass() {
+        this._removeMainPanelClass(StylingClassManager.KEYS.BASE_CLASS);
+    }
+
+    /**
+     * @param {string} colorScheme
+     * @param {number} transparency
+     */
+    enableColoringClass(colorScheme, transparency) {
+        this._addMainPanelClass(
+            StylingClassManager.KEYS.OPACITY_SPECIFIC,
+            'transparent-top-bar-' + colorScheme + '-' + transparency
+        );
+
+        this._addMainPanelClass(
+            StylingClassManager.KEYS.COLOR_SPECIFIC,
+            'transparent-top-bar-' + colorScheme
+        );
+    }
+
+    disableColoringClass() {
+        this._removeMainPanelClass(StylingClassManager.KEYS.OPACITY_SPECIFIC);
+        this._removeMainPanelClass(StylingClassManager.KEYS.COLOR_SPECIFIC);
+    }
+
+    disableAll() {
+        for (const key of Object.keys(this._enabledMainPanelClasses)) {
+            this._removeMainPanelClass(key);
+        }
+    }
 }
